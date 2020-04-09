499a71f254600e7686d961c093790f64bd4febfe
Philip <jhasondeleon@gmail.com>
2020-04-09 17:58:54 -0400

    [Philip] constructor added in model class

--- app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/model/Customer.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/model/Customer.java
@@ -15,4 +15,20 @@ public class Customer {
     private double totalEI;
     private double totalTaxableAmount;
     private double totalTaxPaid;
+
+    public Customer(String firstName, String lastName, String fullName, String gender, String personGender, Date dateOfBirth, Date filingTaxDate, double fedTax, double provTax, double rrspCarryForward, double totalEI, double totalTaxableAmount, double totalTaxPaid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.gender = gender;
+        this.personGender = personGender;
+        this.dateOfBirth = dateOfBirth;
+        this.filingTaxDate = filingTaxDate;
+        this.fedTax = fedTax;
+        this.provTax = provTax;
+        this.rrspCarryForward = rrspCarryForward;
+        this.totalEI = totalEI;
+        this.totalTaxableAmount = totalTaxableAmount;
+        this.totalTaxPaid = totalTaxPaid;
+    }
 }
