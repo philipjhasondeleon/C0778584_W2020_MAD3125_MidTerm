49be13eb841e1f7e4cf83cee298025549225a7ae
Philip <jhasondeleon@gmail.com>
2020-04-09 17:16:08 -0400

    Splash activity added

---
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c0778584_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
