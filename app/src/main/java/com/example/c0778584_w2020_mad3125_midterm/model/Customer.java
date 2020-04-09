d525f4a5338d241c4f79163290f080b11321caca
Philip <jhasondeleon@gmail.com>
2020-04-09 18:07:55 -0400

    [Philip] model class constructor added

--- app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/model/Customer.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/model/Customer.java
@@ -8,17 +8,23 @@ public class Customer {
     private String  lastName;
     private String fullName;
     private  String gender;
-    private  String personGender;
     private Date dateOfBirth;
     private Date filingTaxDate;
     private double fedTax;
     private double provTax;
     private  double grossIncome;
     private double rrspCarryForward;
+    private double rrspContribution;
     private double totalEI;
     private double totalTaxableAmount;
     private double totalTaxPaid;
 
-
-
+    public Customer(String sinNumber, String firstName, String lastName, String gender, double grossIncome, double rrspContribution) {
+        this.sinNumber = sinNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.grossIncome = grossIncome;
+        this.rrspContribution = rrspContribution;
+    }
 }
