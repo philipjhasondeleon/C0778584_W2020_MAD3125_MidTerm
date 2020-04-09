2c246309c2b2be6f15eeac844cddf1007170a09f
Philip <jhasondeleon@gmail.com>
2020-04-09 18:04:08 -0400

    [Philip] model class updated

--- app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/model/Customer.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_midterm/model/Customer.java
@@ -3,6 +3,7 @@ package com.example.c0778584_w2020_mad3125_midterm.model;
 import java.util.Date;
 
 public class Customer {
+    private String sinNumber;
     private String firstName;
     private String  lastName;
     private String fullName;
@@ -10,25 +11,14 @@ public class Customer {
     private  String personGender;
     private Date dateOfBirth;
     private Date filingTaxDate;
-    private double fedTax, provTax;
+    private double fedTax;
+    private double provTax;
+    private  double grossIncome;
     private double rrspCarryForward;
     private double totalEI;
     private double totalTaxableAmount;
     private double totalTaxPaid;
 
-    public Customer(String firstName, String lastName, String fullName, String gender, String personGender, Date dateOfBirth, Date filingTaxDate, double fedTax, double provTax, double rrspCarryForward, double totalEI, double totalTaxableAmount, double totalTaxPaid) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullName = fullName;
-        this.gender = gender;
-        this.personGender = personGender;
-        this.dateOfBirth = dateOfBirth;
-        this.filingTaxDate = filingTaxDate;
-        this.fedTax = fedTax;
-        this.provTax = provTax;
-        this.rrspCarryForward = rrspCarryForward;
-        this.totalEI = totalEI;
-        this.totalTaxableAmount = totalTaxableAmount;
-        this.totalTaxPaid = totalTaxPaid;
-    }
+
+
 }
